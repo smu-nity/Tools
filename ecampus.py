a846e2683b435fe35c423b57c32d40b093f633de
hyunmin <choihm9903@naver.com>
2022-07-16 17:46:26 +0900

    feat: e-campus crawling

---
+++ ecampus.py
@@ -0,0 +1,39 @@
+import os
+import requests
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup as bs
+
+
+# e-campus 로그인
+def login(id, password):
+    user_info = {'username': id, 'password': password}
+    session = requests.Session()
+    request = session.post('https://ecampus.smu.ac.kr/login/index.php', data=user_info)
+
+    # 로그인 성공
+    if request.url == 'https://ecampus.smu.ac.kr/':
+        return session
+    return -1
+
+
+# 수강한 모든 강의 이름 크롤링
+def course(session, year, semester):
+    url = f'https://ecampus.smu.ac.kr/local/ubion/user/?year={year}&semester={semester}'
+    request = session.get(url)
+    source = request.text
+    soup = bs(source, 'html.parser')
+    courses = soup.find_all('a', class_='coursefullname')
+    courses = list(map(lambda s: s.text, courses))
+    return courses
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    session = login(os.environ.get('ID'), os.environ.get('PASSWORD'))
+    courses = []
+
+    for year in range(2015, 2023):
+        for semester in range(1, 3):
+            for type in range(4):
+                courses += course(session, year, f'{semester}{type}')
+    print(courses)
