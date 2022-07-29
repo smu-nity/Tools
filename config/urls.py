e69a50a5b009ce05d53f63bbb1e8ed481fb947b8
hyunmin.0317 <hyunmin.0317@navercorp.com>
2022-07-29 23:29:06 +0900

    feat: main page

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,11 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
