895ed6882f8612327bb81a55d95dceeb13574a96
hyunmin0317 <choihm9903@naver.com>
2023-09-11 15:59:58 +0900

    feat: DjangoPbkdf2PasswordEncoder 구현

--- spring/src/main/kotlin/com/example/spring/encoder/DjangoPbkdf2PasswordEncoder.kt
+++ spring/src/main/kotlin/com/example/spring/encoder/DjangoPbkdf2PasswordEncoder.kt
@@ -1,7 +1,7 @@
 package com.example.spring.encoder
 
-import org.springframework.security.crypto.keygen.BytesKeyGenerator
 import org.springframework.security.crypto.keygen.KeyGenerators
+import org.springframework.security.crypto.keygen.StringKeyGenerator
 import org.springframework.security.crypto.password.PasswordEncoder
 import java.security.GeneralSecurityException
 import java.security.MessageDigest
@@ -14,12 +14,12 @@ class DjangoPbkdf2PasswordEncoder : PasswordEncoder {
     companion object {
         private const val PREFIX = "pbkdf2_sha256"
         private const val SEPARATOR = "\$"
-        private const val ITERATIONS = 180000
+        private const val ITERATIONS = 390000
         private const val HASH_WIDTH = 256
         private const val ALGORITHM = "PBKDF2WithHmacSHA256"
     }
 
-    private val saltGenerator: BytesKeyGenerator = KeyGenerators.secureRandom()
+    private val saltGenerator: StringKeyGenerator = KeyGenerators.string()
 
     private fun base64Decode(string: String): ByteArray {
         return Base64.getDecoder().decode(string)
@@ -31,12 +31,9 @@ class DjangoPbkdf2PasswordEncoder : PasswordEncoder {
 
     override fun encode(rawPassword: CharSequence): String {
         val salt = saltGenerator.generateKey()
-        val hash = encodeWithSalt(rawPassword, salt)
-
+        val hash = encodeWithSalt(rawPassword, salt.toByteArray(Charsets.US_ASCII))
         val encodedHash = base64Encode(hash)
-        val encodedSalt = base64Encode(salt)
-
-        return listOf(PREFIX, ITERATIONS, encodedSalt, encodedHash).joinToString(SEPARATOR)
+        return listOf(PREFIX, ITERATIONS, salt, encodedHash).joinToString(SEPARATOR)
     }
 
     private fun encodeWithSalt(rawPassword: CharSequence, salt: ByteArray): ByteArray {
@@ -50,7 +47,6 @@ class DjangoPbkdf2PasswordEncoder : PasswordEncoder {
             iterations,
             HASH_WIDTH
         )
-
         return try {
             SecretKeyFactory.getInstance(ALGORITHM)
                 .generateSecret(keySpec)
@@ -64,19 +60,13 @@ class DjangoPbkdf2PasswordEncoder : PasswordEncoder {
         if (!partsEncodedPassword.startsWith(PREFIX)) {
             throw IllegalArgumentException("Encoded password does not start with: $PREFIX")
         }
-
         val parts = partsEncodedPassword.split(SEPARATOR)
         if (parts.size != 4) {
             throw IllegalArgumentException("The encoded password format does not have 4 parts")
         }
-
         val iterations = parts[1].toInt()
-        val salt = base64Decode(parts[2])
+        val salt = parts[2].toByteArray(Charsets.US_ASCII)
         val hash = base64Decode(parts[3])
-
-        return MessageDigest.isEqual(
-            hash,
-            encodeWithSaltAndIterations(rawPassword, salt, iterations)
-        )
+        return MessageDigest.isEqual(hash, encodeWithSaltAndIterations(rawPassword, salt, iterations))
     }
 }
--- spring/src/test/kotlin/com/example/spring/encoder/EncoderTest.kt
+++ spring/src/test/kotlin/com/example/spring/encoder/EncoderTest.kt
@@ -6,8 +6,9 @@ class EncoderTest {
     @Test
     fun main() {
         val djangoPbkdf2PasswordEncoder = DjangoPbkdf2PasswordEncoder()
-        val password = djangoPbkdf2PasswordEncoder.encode("password")
-        System.out.println(password)
-        System.out.println(djangoPbkdf2PasswordEncoder.matches("password", password))
+        val password = djangoPbkdf2PasswordEncoder.encode("testtest")
+        val encoding = "pbkdf2_sha256\$390000\$8ZcmAeOFPm4ZY2aLYghxzI\$ufspbGraVRYQIv+TrdJDH5jj2V3aMbz+wXx3qgJ8gbE="
+        System.out.println(djangoPbkdf2PasswordEncoder.matches("testtest", password))
+        System.out.println(djangoPbkdf2PasswordEncoder.matches("testtest", encoding))
     }
 }
